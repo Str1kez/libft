85ef9f6880a4514024714ade99a1c25734aa65b3
Thao Nessround <tnessrou@ge-b1.kzn.21-school.ru>
2021-04-25 21:11:13 +0300

    atoi and strnstr, need MEMs

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,60 +11,56 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
+// #include <stdio.h>
+// #include <string.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	char	*help;
-	char	*start;
-	size_t	max;
-	size_t	current;
+	char	*save;
 
-	start = (char *)needle;
-	help = NULL;
-	max = 0;
-	current = 0;
-	while (*haystack)
+	save = (char *)haystack;
+	help = (char *)needle;
+	while (haystack && (*haystack || (!*haystack && !*needle)) && len)
 	{
-		if (*start == *haystack)
+		if (*haystack == *help || !*help)
 		{
-			current++;
-			if (current > max)
+			save = (char *)haystack;
+			while (*haystack == *help && len && *haystack)
 			{
-				max = current;
-				help = (char *)haystack - max + 1;
+				help++;
+				haystack++;
+				len--;
 			}
-			if (max == len)
-				return (help);
-			start++;
-		}
-		else if (current)
-		{
-			current = 0;
-			start = (char *)needle;
+			if (!*help)
+				return (save);
+			help = (char *)needle;
 		}
 		haystack++;
+		if (len)
+			len--;
 	}
-	return (help);
+	return (NULL);
 }
 
-// TODO: Doesn't work correctly
+// TODO: NEED CHECK
 
-int	main()
-{
-	char *s_src = "r43trebvabh5rebva";
-	char *needle = "rebva";
-	// char *res, *res;
+// int	main()
+// {
+// 	char *s_src = "djfafd";
+// 	char *needle = "";
+// 	// char *res, *res;
 
-	// int t = 1;
-	// for (int i = -10; i < 200; i++)
-	// {
-	// 	res = ft_strrchr(s_src, i);
-	// 	res2 = strrchr(s_src, i);
-	// 	t *= res == res2;
-	// 	printf("%s\t", res);
-	// 	printf("%s\n", res2);
-	// }
-	// printf("%d\n", t);
-	printf("%s\n", ft_strnstr(s_src, needle, 5));
-}
+// 	// int t = 1;
+// 	// for (int i = -10; i < 200; i++)
+// 	// {
+// 	// 	res = ft_strrchr(s_src, i);
+// 	// 	res2 = strrchr(s_src, i);
+// 	// 	t *= res == res2;
+// 	// 	printf("%s\t", res);
+// 	// 	printf("%s\n", res2);
+// 	// }
+// 	// printf("%d\n", t);
+// 	printf("%s\n", ft_strnstr(s_src, needle, 3));
+// 	printf("%s\n", strnstr(s_src, needle, 3));
+// }
---
+++ libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnessrou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 18:58:44 by tnessrou          #+#    #+#             */
+/*   Updated: 2021/04/25 18:58:46 by tnessrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
